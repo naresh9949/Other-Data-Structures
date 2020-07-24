f68d301c5b65d081d104f4fddb1f000154a12621
Naresh Kollipora <53150348+naresh9949@users.noreply.github.com>
2020-07-24 03:27:00 -0700

    Update maxHeap.cpp

--- maxHeap.cpp
+++ maxHeap.cpp
@@ -1,173 +1,150 @@
 #include<iostream>
-#include<list>
-#include<map>
+#include<algorithm>
+#include<vector>
 using namespace std;
 
-struct MaxHeapNode
+struct node
 {
-	int vertex;
-	int distance;
+    int value;
+    char name;
 };
 
-class MaxHeap
+class MinHeap
 {
-	int capacity;
-	int size;
-	MaxHeapNode *maxheap;
-	map<int , MaxHeapNode> map;
-
-	
-public:
-	void Heapify(int i);
-	MaxHeap(int c);
-	int Capacity();
-	int Size();
-	//MinHeapNode Create_Node(int v,int d);
-	void Heapi();
-	void Swap(MaxHeapNode &m1,MaxHeapNode &m2);
-	void Add_Node(int v,int d);
-	bool IsThere(int v);
-	MaxHeapNode Extract_Max();
-
-	bool IsEmpty()
-	{
-		return this->size;
-	}
-
-	void Show();
-
+    int size;
+    std::vector<node> minHeap;
+    
+    public:
+    
+     MinHeap()
+    {
+        size = 0;
+    }
+    
+    void Insert(char name,int value)
+    {
+        node newNode;
+        newNode.name = name;
+        newNode.value = value;
+        
+        this->size++;
+        
+        minHeap.push_back(newNode);
+        
+        int cur = size-1;
+        
+        int parent = (cur-1)/2;
+        
+        while(cur>0)
+        {
+            if(minHeap[parent].value<minHeap[cur].value)
+            {
+                swap(minHeap[parent],minHeap[cur]);
+                cur = parent;
+                parent = (cur-1)/2;
+            }
+            else
+            return;
+        }
+    }
+    
+    
+    char Extraxt_Min()
+    {
+        char min = minHeap[0].name;
+        
+        minHeap[0] = minHeap[size-1];
+        
+        minHeap.pop_back();
+        
+        size--;
+        
+        int cur_pos = 0;
+        int left = (2*cur_pos)+1;
+        int right = (2*cur_pos)+2;
+        
+        while(left<size || right<size)
+        {
+            if(left<size && right<size)
+            {
+                if(minHeap[left].value > minHeap[right].value && minHeap[left].value > minHeap[cur_pos].value)
+                {
+                    swap(minHeap[left],minHeap[cur_pos]);
+                    
+                    cur_pos = left;
+                    
+                    left = (2*cur_pos)+1;
+                    right = (2*cur_pos)+2;
+                }
+                
+                else
+                {
+                    swap(minHeap[right],minHeap[cur_pos]);
+                    
+                    cur_pos = right;
+                    
+                    left = (2*cur_pos)+1;
+                    right = (2*cur_pos)+2;
+                }
+                
+            }
+            
+            else if(left<size && minHeap[left].value > minHeap[cur_pos].value)
+            {
+                 swap(minHeap[left],minHeap[cur_pos]);
+                    
+                    cur_pos = left;
+                    
+                    left = (2*cur_pos)+1;
+                    right = (2*cur_pos)+2;
+            }
+            
+            else if(right<size && minHeap[right].value > minHeap[cur_pos].value)
+            {
+                 swap(minHeap[right],minHeap[cur_pos]);
+                    
+                    cur_pos = right;
+                    
+                    left = (2*cur_pos)+1;
+                    right = (2*cur_pos)+2;
+            }
+            
+            else
+            return min;
+            
+            
+        }
+        
+        return min;
+    }
+    
+    
+    void Show()
+    {
+        for(int i=0;i<size;i++)
+        cout<<"("<<minHeap[i].name<<","<<minHeap[i].value<<")"<<" ";
+        cout<<endl;
+    }
+    
 };
 
-
-MaxHeap :: MaxHeap(int c)
-	{
-		this->capacity = c;
-		this->size = 0;
-		this->maxheap = new MaxHeapNode[c];
-	}
-
-
-int MaxHeap :: Capacity()
-{
-	return this->capacity;
-}
-
-int MaxHeap :: Size()
-{
-	return this->size;
-}
-
-
-
-
-void MaxHeap :: Add_Node(int v,int d)
-{
-	MaxHeapNode mm;
-	mm.vertex = v;
-	mm.distance = d;
-	if(size+1<capacity){
-		maxheap[size] = mm;
-		size++;
-		map[v] = mm;
-	}
-	else
-		cout<<"Heap if FULL!"<<endl;
-}
-
-
-void MaxHeap :: Swap(MaxHeapNode &m1,MaxHeapNode &m2)
-{
-	MaxHeapNode m = m1;
-	m1 = m2;
-	m2 = m;
-}
-
-MaxHeapNode MaxHeap :: Extract_Max()
-{
-	MaxHeapNode m = this->maxheap[0];
-
-	MaxHeapNode m1 = this->maxheap[size-1];
-
-	this->maxheap[0] = m1;
-	this->maxheap[size] = m;
-
-	size--;
-
-	map[m.vertex].distance = INT_MIN;
-
-	Heapify(0);
-
-	return m;
-
-
-}
-
-
-
-void MaxHeap :: Heapify(int i)
-{
-	int largest = i;
-	int l = (2*i)+1;
-	int r = (2*i)+2;
-
-	if(l<this->size && this->maxheap[l].distance > this->maxheap[largest].distance)
-		largest = l;
-
-	if(r<this->size && this->maxheap[r].distance > this->maxheap[largest].distance)
-		largest = r;
-
-	if(i!=largest)
-	{
-		Swap(this->maxheap[i],this->maxheap[largest]);
-		Heapify(largest);
-	}
-
-}
-
-void MaxHeap :: Heapi()
-{
-	for(int i=((this->size+1)/2)-1;i>=0;i--)
-		Heapify(i);
-	//this->Show();
-}
-
-void MaxHeap:: Show()
-{
-	for (int i = 0; i < size; i++)
-	{
-		cout<<this->maxheap[i].vertex<<":"<<this->maxheap[i].distance<<" ";
-	}
-	cout<<endl;
-}
-
-
-bool MaxHeap:: IsThere(int v)
-{
-	//cout<<map[v].vertex<<" :"<<map[v].distance<<endl;
-
-	if(map[v].distance == INT_MIN)
-		return 0;
-	return 1;
-} 
 int main()
 {
-	MaxHeap m(7);
-	m.Add_Node(1,20);
-	//m.Show();
-	m.Add_Node(2,34);
-	//m.Show();
-	m.Add_Node(3,4);
-	//m.Show();
-	m.Add_Node(4,2);
-	m.Show();
-	m.Heapi();
-	m.Show();
-	cout<<endl;
-		cout<<endl;
-	
+    MinHeap m;
+    
+    m.Insert('A',20);
+    m.Insert('b',30);
+    m.Insert('c',40);
+    m.Insert('d',10);
+    m.Insert('f',50);
+    //m.Insert('k',2);
+    
+    m.Show();
+    
+    cout<<m.Extraxt_Min()<<endl;
 
 
-	MaxHeapNode M = m.Extract_Max();
-	cout<<M.vertex<<" :"<<M.distance<<endl;
-
+    
+    m.Show();
+    
 }
